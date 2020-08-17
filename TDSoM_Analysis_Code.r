b3cb49bfb05ed7011f1393fe60f185826ea7ee74
Cliff Workman <36338172+cliffworkman@users.noreply.github.com>
2020-08-17 15:53:18 -0400

    Add files via upload

--- TDSoM_Analysis_Code.r
+++ TDSoM_Analysis_Code.r
@@ -4,12 +4,17 @@
 require("Hmisc")
 require("xlsx")
 require("psych")
+require("tidyverse")
+require("sjPlot")
+require("sjstats")
+require("lmerTest")
 
 #########################################################################################################################
 
 ## Load data for reliability analysis
-setwd("C:\\Data\\")
-tdsom_mcs <- read.csv('TDSoM_MoralCon_Data.csv')
+#setwd("C:\\Data\\")
+setwd("C:\\Users\\cliff\\Dropbox\\Dropbox\\02_UChicago\\Dark Side of Morality MS\\Data")
+tdsom_mcs <- read.csv('TDSoM_MCS_Data.csv')
 if ("tdsom_mcs" %in% search()) {
   detach(tdsom_mcs)
 }
@@ -37,5 +42,44 @@ corr_out.r = data.frame(correlations$r); corr_out.p = data.frame(correlations$P)
 write.xlsx(corr_out.r,file=paste0("TDSoM_Correlations.xlsx"),sheetName="spearmans_rho")
 write.xlsx(corr_out.p,file=paste0("TDSoM_Correlations.xlsx"),sheetName="p-values", append=TRUE)
 write.xlsx(corr_out.n,file=paste0("TDSoM_Correlations.xlsx"),sheetName="Ns", append=TRUE)
+
+#########################################################################################################################
+
+## Load data for Figure 4
+apvt_beh <- read.csv('TDSoM_Longform_Data.csv')
+apvt_mcs <- read_csv('TDSoM_MCSfMRI_Data.csv')
+apvt <- apvt_beh %>%
+  left_join(apvt_mcs %>% 
+              pivot_longer(bomb:welfare, names_to = "Issue", values_to = "MC"),
+            by = c("Subject" = "Subject", "Issue"))
+apvt <- apvt %>%
+  mutate(ProtestView = case_when(
+    str_detect(Thumbs, "up")   ~ "Support",
+    str_detect(Thumbs, "down") ~ "Oppose",
+    TRUE ~ NA_character_
+  )) %>%
+  mutate(Support.c = Support - 4) %>%
+  mutate(ProtestCongruent = case_when(
+    Support.c > 0 & ProtestView == "Support" ~  1,
+    Support.c < 0 & ProtestView == "Oppose"  ~  1,
+    Support.c < 0 & ProtestView == "Support" ~ -1,
+    Support.c > 0 & ProtestView == "Oppose"  ~ -1,
+    Support.c == 0 ~ 0
+  )) %>%
+  mutate(MC.c = MC - mean(MC, na.rm=TRUE))
+apvt$Subject <- factor(apvt$Subject)
+apvt$Issue   <- factor(apvt$Issue)
+
+# Model MCS and appropriateness ratings for fMRI issues
+apvt_mod <- lmer(Appropriate ~ Support.c + MC.c + ProtestCongruent + Support.c:MC.c + Support.c:ProtestCongruent + MC.c:ProtestCongruent + (1|Subject) + (1|Issue), data = apvt)
+summary(apvt_mod)
+effectsize::standardize_parameters(apvt_mod)
+
+# Generate plot
+plot_model(apvt_mod, type="pred", terms = c("ProtestCongruent"), colors = "v") +
+  labs(title = element_blank(), y="Appropriateness Rating", x="Protest Congruency") +
+  theme_classic() +
+  geom_hline(yintercept = mean(apvt$Appropriate), linetype = "dashed", alpha=.3)
+ggsave("APVT_Appropriate_SocialSupp.png", width=3.25, height=3, units = "in")
 
 #########################################################################################################################
